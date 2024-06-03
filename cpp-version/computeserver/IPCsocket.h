2e8561364ecf17fe3783da06f6bb0b634460b5ec
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2024-06-03 17:05:35 -0400

    Cleanup

--- cpp-version/computeserver/IPCsocket.h
+++
@@ -1,6 +0,0 @@
-#ifndef IPCSOCKET_H
-#define IPCSOCKET_H
-
-int make_named_socket(const char* filename);
-
-#endif
--- cpp-version/run.sh
+++
@@ -1,5 +0,0 @@
-# make sure you run npm run build in webserver/ first
-cd webserver
-npm run start & disown
-cd ../computeserver
-./ComputeServer 
